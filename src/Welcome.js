e781a10cb1a1b6d44cffb8c545f016fcc6d45c6a
Krithik <krithikinbox@gmail.com>
2024-01-22 18:26:02 -0600

    added welcome

---
+++ src/Welcome.js
@@ -0,0 +1,18 @@
+import './Welcome.css';
+import React from "react";
+
+function Welcome() {
+    return (
+        
+    <div>
+      <div className="mainDiv">
+        <div className="row">
+          <h1>Home Page</h1>
+          <p className='para'>{"This is a website that you can sign up and log in to. Once you sign up, you can use the same username and password to log in to the main page. There will be a variety of other websites once you log in with an username and password. Click the 3 lines in the top-left of the screen to navigate through pages. ~ Krithik Senthilkumar"}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Welcome;
