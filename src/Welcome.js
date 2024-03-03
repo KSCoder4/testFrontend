68d3da50d87f2c2888cb1c4e080015ec05d24ab2
Krithik <krithikinbox@gmail.com>
2024-03-03 09:03:17 -0600

    changed welcome

--- src/Welcome.js
+++ src/Welcome.js
@@ -7,7 +7,7 @@ function Welcome() {
         <div className="mainDiv">
           <div className="row">
             <h1>Home Page</h1>
-            <p className='para'>{"This is a website that you can sign up and log in to. Once you sign up, you can use the same username and password to log in to the main page. There will be a variety of other websites once you log in with an username and password. Click the 3 lines in the top-left of the screen to navigate through pages. ~ Krithik Senthilkumar"}</p>
+            <p className='para'>{"This is a website that you can sign up and log in to. Once you sign up, you can use the same username and password to log in to the main page. There will be a variety of other pages in the navigation bar once you log in with an username and password. Click the 3 lines in the top-left of the screen to navigate through pages. ~ Krithik Senthilkumar"}</p>
           </div>
         </div>
       </div>
